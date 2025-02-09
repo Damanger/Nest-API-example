f20eceb59dfc31af7bb2aadd12c71b287ac6f94e
Damanger <omar.cruzr97@hotmail.com>
2025-02-08 23:21:39 -0600

    updating

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,13 +14,13 @@ export class AppModule {}*/
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MessageController } from './message/message.controller';
+import { MessageGateway } from './message/message.gateway';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
   ],
-  controllers: [MessageController],
-  providers: [],
+  controllers: [],
+  providers: [MessageGateway],
 })
 export class AppModule {}
--- src/message/message.gateway.ts
+++ src/message/message.gateway.ts
@@ -3,20 +3,17 @@ import {
   WebSocketServer,
   SubscribeMessage,
   MessageBody,
-  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessageGateway {
   @WebSocketServer()
   server: Server;
 
+  // Handle incoming messages
   @SubscribeMessage('sendMessage')
-  handleMessage(
-    @MessageBody() message: string,
-    @ConnectedSocket() client: Socket,
-  ): void {
+  handleMessage(@MessageBody() message: string): void {
     console.log('Received message:', message);
     this.server.emit('receiveMessage', message);
   }
