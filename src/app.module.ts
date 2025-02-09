509be972636b9736e59e0c789d02b102dc916be9
Damanger <omar.cruzr97@hotmail.com>
2025-02-08 23:32:31 -0600

    updating

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,3 @@
-/*import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MessageController } from './message/message.controller';
-import { MessageGateway } from './message/message.gateway';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot(),
-  ],
-  controllers: [MessageController],
-  providers: [MessageGateway],
-})
-export class AppModule {}*/
-
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MessageGateway } from './message/message.gateway';
